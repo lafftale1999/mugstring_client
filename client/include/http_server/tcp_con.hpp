3ed9ee4e468ef7451dfc75e58bc896d9558fac68
Carl Broman <lafftale1999@gmail.com>
2026-05-05 17:16:33 +0200

    updated to a more lightweight approach

--- client/include/http_server/tcp_con.hpp
+++ client/include/http_server/tcp_con.hpp
@@ -1,7 +1,6 @@
 #ifndef LTALE_SOCKET_TCP_HPP_
 #define LTALE_SOCKET_TCP_HPP_
 
-#include <iostream>
 #include <vector>
 #include <optional>
 #include <cerrno>
@@ -16,7 +15,7 @@
 
 namespace networking::tcp {
 
-#define L_TCP_SOCKET_BUFFER_SIZE        2048    /**< Maximum reading size for tcp socket */
+static constexpr size_t TCP_BUFFER_SIZE = 2048;
 
 using byte_array = std::vector<char>;
 
@@ -26,95 +25,55 @@ enum class L_TCP_SOCKET_RES {
     BUFFER_OVERFLOW,
     BLOCKING,
     CLIENT_CON_CLOSED,
-    UNKOWN_ERROR  
+    UNKNOWN_ERROR
 };
 
 /**
- * Virtual base class representing a TCP-connection
+ * Reads up to dataSize bytes from the socket into dataOut.
+ * dataOut is cleared before reading begins.
+ *
+ * @return L_TCP_SOCKET_RES
  */
-class ConBase {
+L_TCP_SOCKET_RES readData(int fd, byte_array& dataOut, size_t dataSize);
+
+/**
+ * Sends all of dataIn through the socket.
+ *
+ * @return L_TCP_SOCKET_RES
+ */
+L_TCP_SOCKET_RES sendData(int fd, const byte_array& dataIn);
+
+struct Client {
+    int             fd;
+    sockaddr_in     addr;
+    std::string     buff;
+};
+
+/**
+ * Class for server-side TCP logic
+ */
+class ServerCon {
 public:
-    ConBase() = default;
-    virtual ~ConBase() = 0;
+    ServerCon(int port, size_t maxConnections);
+    ~ServerCon();
 
-    int getPort() const; 
-    void setPort(int port);
+    /**
+     * Wraps accept() and returns a ClientCon on success.
+     *
+     * @return optionally returns a ClientCon
+     */
+    std::optional<Client> acceptConnection();
 
-protected:
+    int getPort() const;
+
+private:
+    /** Initializes the server socket. Throws std::runtime_error on failure. */
+    void init();
+    
     int             socket_;
     sockaddr_in     addr_;
     int             port_;
-};
-
-/**
- * Class representing a Client connection over TCP. Only accepts non-blocking connections
- */
-class ClientCon : public ConBase {
-public:
-    /**
-     * Constructing the ConBase class.
-     * 
-     * @param socket Filedescriptor for the socket, this socket need to be declared as
-     * non-blocking.
-     * 
-     * @param addr Already Initialized sockaddr_in
-     */
-    ClientCon(int socket, sockaddr_in addr);
-    virtual ~ClientCon() override;
-
-    /**
-     * Reads data from the socket until it is maxed out in dataSize or if the socket
-     * has nothing more to read.
-     * 
-     * @param dataOut A char array to stream bytes from,
-     * @param dataSize A set size of the data buffer to easily restrict how much to read.
-     * 
-     * @return L_TCP_SOCKET_RES
-     */
-    L_TCP_SOCKET_RES readData(byte_array& dataOut, size_t dataSize);
-
-    /**
-     * Sends the data through the specified socket.
-     * 
-     * @param dataIn The data to send on the socket.
-     * 
-     * @return L_TCP_SOCKET_RES
-     */
-    L_TCP_SOCKET_RES sendData(const byte_array& dataIn);
-
-    void setIP();
-    const char* getIP() const;
-
-private:
-    char            ip_[INET_ADDRSTRLEN];
-    int             timeout_;
-};
-
-/**
- * Class for server based logic
- */
-class ServerCon : public ConBase {
-public:
-    ServerCon(int port, size_t maxConnections);
-    virtual ~ServerCon() override;
-
-    /**
-     * Wraps the accept() function and returns a instance of the ClientCon.
-     * 
-     * @return optionally returns a ClientCon when read properly
-     */
-    std::optional<ClientCon> acceptConnection();
-
-private:
-
-    /**
-     * Initializes the ServerCon
-     * 
-     * @return 0 on success, 1 on failure.
-     */
-    uint8_t init();
-
-    size_t maxConnections_;
+    size_t          maxConnections_;
 };
 }
 
--- client/src/http_server/tcp_con.cpp
+++ client/src/http_server/tcp_con.cpp
@@ -1,194 +1,135 @@
 #include "../../include/http_server/tcp_con.hpp"
 
 #include <array>
-#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include <stdexcept>
 
 #include <fcntl.h>
 
 namespace networking::tcp {
 
-/**
- * ------------ SOCKET BASE ------------
- */
-
-ConBase::~ConBase() {
-    if (this->socket_ > 0) {
-        close(this->socket_);
-    }
-}
-
-int ConBase::getPort() const {
-    return this->port_;
-}
-
-void ConBase::setPort(int port) {
-    if (port <= 0) {
-        throw std::invalid_argument("Port number cant be less than or equal to 0");
-    }
-
-    port_ = port;
-}
-
-/**
- * ------------ CLIENT SOCKET ------------
- */
-
-ClientCon::ClientCon(int s, sockaddr_in addr) {
-    if (s <= 0) {
-        throw std::runtime_error("Socket is not valid");
-    }
-
-    this->socket_ = s;
-    this->addr_ = addr;
-    this->setIP();
-    this->setPort(ntohs(addr_.sin_port));
-
-    if (fcntl(this->socket_, F_SETFL, O_NONBLOCK) < 0) {
-        throw std::runtime_error("Failed to set socket to non-blocking");
-    }
-}
-
-ClientCon::~ClientCon() {}
-
-L_TCP_SOCKET_RES ClientCon::readData(byte_array& dataOut, size_t dataSize) {
-    std::array<char, L_TCP_SOCKET_BUFFER_SIZE> buff;
+L_TCP_SOCKET_RES readData(int fd, byte_array& dataOut, size_t dataSize) {
+    dataOut.clear();
+    std::array<char, TCP_BUFFER_SIZE> buff;
     size_t totalBytesRead = 0;
 
     while (totalBytesRead < dataSize) {
-        std::memset(buff.data(), 0, buff.size());
-
-        ssize_t bytesRead = recv(this->socket_, buff.data(), buff.size(), 0);
+        size_t toRead = std::min(dataSize - totalBytesRead, buff.size());
+        ssize_t bytesRead = recv(fd, buff.data(), toRead, 0);
 
         if (bytesRead == -1) {
             if (errno == EWOULDBLOCK || errno == EAGAIN) {
                 return L_TCP_SOCKET_RES::BLOCKING;
             }
-            return L_TCP_SOCKET_RES::UNKOWN_ERROR;
-            
+            return L_TCP_SOCKET_RES::UNKNOWN_ERROR;
         }
 
-        if ((totalBytesRead + bytesRead) < dataSize) {
-            if (bytesRead > 0) {
-                dataOut.insert(
-                    dataOut.end(), 
-                    std::make_move_iterator(buff.begin()),
-                    std::make_move_iterator(buff.begin() + bytesRead)
-                );
-                totalBytesRead += bytesRead;
-            } else if (bytesRead == 0) {
-                return L_TCP_SOCKET_RES::CLIENT_CON_CLOSED;
-            } else {
-                throw std::runtime_error("Socket entered unexpected state");
-            }
-        } else {
-            return L_TCP_SOCKET_RES::BUFFER_OVERFLOW;
+        if (bytesRead == 0) {
+            return L_TCP_SOCKET_RES::CLIENT_CON_CLOSED;
         }
+
+        dataOut.insert(dataOut.end(), buff.begin(), buff.begin() + bytesRead);
+        totalBytesRead += bytesRead;
     }
 
     return L_TCP_SOCKET_RES::SUCCESSFUL_READ;
 }
 
-L_TCP_SOCKET_RES ClientCon::sendData(const byte_array& dataIn) {
+L_TCP_SOCKET_RES sendData(int fd, const byte_array& dataIn) {
+    const char* pos = dataIn.data();
     size_t toSend = dataIn.size();
     size_t totalBytesSent = 0;
 
-    char* pos = (char*)dataIn.data();
-
     while (totalBytesSent < dataIn.size()) {
-        ssize_t bytesSent = send(this->socket_, pos, toSend, 0);
-        
+        ssize_t bytesSent = send(fd, pos, toSend, 0);
+
         if (bytesSent == -1) {
             if (errno == EWOULDBLOCK || errno == EAGAIN) {
                 return L_TCP_SOCKET_RES::BLOCKING;
             }
-
-            return L_TCP_SOCKET_RES::UNKOWN_ERROR;
+            return L_TCP_SOCKET_RES::UNKNOWN_ERROR;
         }
 
-        if (bytesSent > 0) {
-            totalBytesSent += bytesSent;
-            pos += bytesSent;
-            toSend -= bytesSent;
-        } else if (bytesSent == 0) {
+        if (bytesSent == 0) {
             return L_TCP_SOCKET_RES::CLIENT_CON_CLOSED;
-        } else {
-            throw std::runtime_error("Socket entered unexpected state");
         }
+
+        totalBytesSent += bytesSent;
+        pos += bytesSent;
+        toSend -= bytesSent;
     }
 
     return L_TCP_SOCKET_RES::SUCCESSFUL_SEND;
 }
 
-void ClientCon::setIP() {    
-    inet_ntop(AF_INET, &addr_.sin_addr, ip_, INET_ADDRSTRLEN);
-}
-
-const char* ClientCon::getIP() const {
-    return this->ip_;
-}
-
-/**
- * ------------ SERVER SOCKET ------------
- */
-
 ServerCon::ServerCon(int port, size_t maxConnections) {
     if (port <= 0) {
-        throw std::runtime_error("Invalid port numbers");
+        throw std::runtime_error("Portnumber can't be less than 0");
     }
 
-    this->port_ = port;
-    this->maxConnections_ = maxConnections;
+    port_ = port;
+    maxConnections_ = maxConnections;
+    init();
+}
 
-    if (this->init() != 0) {
-        throw std::runtime_error("Failed to initialize server");
+ServerCon::~ServerCon() {
+    if (socket_ > 0) {
+        close(socket_);
     }
 }
 
-ServerCon::~ServerCon() {}
-
-std::optional<ClientCon> ServerCon::acceptConnection() {
+std::optional<Client> ServerCon::acceptConnection() {
     sockaddr_in clientAddress;
     socklen_t clientSize = sizeof(clientAddress);
 
-    int clientSocket = accept(this->socket_, (sockaddr *)&clientAddress, &clientSize);
+    int clientSocket = accept(socket_, (sockaddr*)&clientAddress, &clientSize);
 
-    if (clientSocket <= 0) {
-        std::cout << "Failed to accept incomming connection" << std::endl;
+    if (clientSocket < 0) {
+        std::cerr << "Failed to accept incoming connection: " << strerror(errno) << '\n';
         return std::nullopt;
     }
 
-    return ClientCon(clientSocket, clientAddress);
+    if (fcntl(clientSocket, F_SETFL, O_NONBLOCK) != 0) {
+        std::cerr << "Failed to set socket to nonblocking: " << strerror(errno) << std::endl;
+        return std::nullopt;
+    }
+
+    Client client = {
+        .fd = clientSocket,
+        .addr = clientAddress,
+        .buff = std::string()
+    };
+
+    return client;
 }
 
-uint8_t ServerCon::init() {
-    this->socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+int ServerCon::getPort() const {
+    return this->port_;
+}
 
-    if (this->socket_ < 0) {
-        std::cout << "Failed to create socket" << std::endl;
-        return 1;
+void ServerCon::init() {
+    socket_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (socket_ < 0) {
+        throw std::runtime_error(std::string("Failed to create socket: ") + strerror(errno));
     }
 
     int opt = 1;
-    if (setsockopt(this->socket_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) != 0) {
-        std::cout << "Error when setting socket options: " << strerror(errno) << std::endl;
-        return 1;
+    if (setsockopt(socket_, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)) != 0) {
+        throw std::runtime_error(std::string("Failed to set socket options: ") + strerror(errno));
     }
 
     addr_.sin_family = AF_INET;
     addr_.sin_port = htons(port_);
     addr_.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(this->socket_, (const sockaddr*)&(this->addr_), sizeof(this->addr_)) != 0) {
-        std::cout << "Failed to bind socket: " << strerror(errno) << std::endl;
-        return 1; 
+    if (bind(socket_, (const sockaddr*)&addr_, sizeof(addr_)) != 0) {
+        throw std::runtime_error(std::string("Failed to bind socket: ") + strerror(errno));
     }
 
-    if (listen(this->socket_, this->maxConnections_) != 0) {
-        std::cout << "Failed to start listen: " << strerror(errno) << std::endl;
-        return 1;
+    if (listen(socket_, maxConnections_) != 0) {
+        throw std::runtime_error(std::string("Failed to listen: ") + strerror(errno));
     }
-
-    return 0;
 }
 
 }
