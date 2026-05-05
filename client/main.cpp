846cdfda7086ec074b1207c390508e55c8366532
Carl Broman <lafftale1999@gmail.com>
2026-05-05 17:24:37 +0200

    Makefile is tested, and the boardspecific compiler works as intened

--- client/main.cpp
+++ client/main.cpp
@@ -4,26 +4,30 @@
 
 int main(void) {
     
-    networking::tcp::ServerCon server(8080, 20);
+    networking::ServerCon server(8080, 20);
 
     auto client = server.acceptConnection();
 
     if (client.has_value()) {
-        networking::tcp::byte_array data();
-        auto res = client->readData(data, data.size());
+        networking::tcp::byte_array data;
+        auto res = networking::tcp::readData(client->fd, data, 2000);
 
         if (res == networking::tcp::L_TCP_SOCKET_RES::SUCCESSFUL_READ ||
             res == networking::tcp::L_TCP_SOCKET_RES::BLOCKING) {
-                std::cout << "Incoming data: ";
-                for (const char c : data) {
-                    std::cout << c;
-                }
-
-                std::cout << std::endl;
+            std::cout << "Incoming data: ";
+            for (const char c : data) {
+                std::cout << c;
             }
-        
-        
+            std::cout << '\n';
+        } else {
+            std::cerr << "Failed to read incoming request: " << (int)res << std::endl;
+            std::cerr << "Reason: " << strerror(errno) << std::endl;
+        }
+
         res = client->sendData(data);
+        if (res != networking::tcp::L_TCP_SOCKET_RES::SUCCESSFUL_SEND) {
+            std::cerr << "Failed to send data\n";
+        }
     }
 
     return 0;
