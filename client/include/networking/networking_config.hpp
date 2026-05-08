c56c322a8ed39d0f61a4ea5a4f50f4808dab7081
Carl Broman <lafftale1999@gmail.com>
2026-05-08 21:46:06 +0200

    Added configuration file for the networking portion

---
+++ client/include/networking/networking_config.hpp
@@ -0,0 +1,18 @@
+#ifndef LTALE_NETW_CONFIG_HPP_
+#define LTALE_NETW_CONFIG_HPP_
+
+/**
+ * Server specific configurations
+ */
+#define LTALE_LISTEN_MAX_CONNECTIONS
+#define LTALE_SERVER_NAME                   "lafftale"
+
+/**
+ * HTTP request and response configurations
+ */
+#define LTALE_HTTP_HEADER_MAX_SIZE_BYTES    256
+#define LTALE_HTTP_HEADER_MAX_AMOUNT_N      50
+#define LTALE_HTTP_BODY_MAX_SIZE_BYTES      
+
+
+#endif
